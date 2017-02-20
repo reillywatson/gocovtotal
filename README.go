af4f732b724d2fc6d37c6791d7d84d8fb2fcaf72
Reilly Watson <reillywatson@gmail.com>
2017-02-20 10:28:09 -0500

    formatting

--- README.go
+++ README.go
@@ -1,6 +1,7 @@
 gocovtotal
+----------
 
 Measures coverage percentage in go cover profiles (ie go test -coverprofile=cover.out). go test will show you that information, but if you're merging multiple coverage profiles (using gocovmerge, for instance), you won't have that information.
 
-Usage: gocovtotal cover.out
+Usage: `gocovtotal cover.out`
 
